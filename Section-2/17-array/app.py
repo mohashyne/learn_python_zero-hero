2004842d11347d8016a9c90c1e8924123f3e7448
MSA <crystalbluetech@gmail.com>
2024-12-02 11:28:58 +0100

    arrays

---
+++ Section-2/17-array/app.py
@@ -0,0 +1,13 @@
+# Array, why and when do we need it (REMEMBER IF YOU DON'T HAVE PERFORMANCE ISSUES, YOU DON'T NEED TO USE ARRAY)
+# DON'T SOLVE A PROBLEM THAT YOU DON'T HAVE YET
+
+# Array is a collection of similar data types. It is a fixed size data structure which means no element can be added or removed after the declaration of an array. It is used to store a collection of data in a sequential manner. It is used to store a collection of data in a sequential manner.
+# It is a collection of items which can be of the same kind or of different kinds. It 
+# is a collection of items which can be of the same kind or of different kinds.
+
+# When do we need an array? instead of using a list
+# we use arrays when we have a huge amount of data and we need to perform the same operation on all the data, for example, we have a list of 1000 numbers and we need to multiply all the numbers by 2, we can use an array to store the numbers and perform the operation on all the numbers at once, tto avoid writing a loop to iterate over the list and perform the operation on each number one by one. so as tto increase the performance of the program, we can use an array instead of a list.
+
+
+
+[]
