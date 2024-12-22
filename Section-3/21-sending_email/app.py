13df0ea1a331d4610ceece8eb36483f705476737
MSA <crystalbluetech@gmail.com>
2024-12-22 18:54:07 +0100

    sending email

--- Section-3/21-sending_email/app.py
+++ Section-3/21-sending_email/app.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 import smtplib
 
@@ -7,6 +9,7 @@ message["from"] = "Muhammad Msa"
 message["to"] = "alkaline4peace@gmail.com"
 message["subject"] = "This is a test"
 message.attach(MIMEText("Body"))
+message.attach(MIMEImage(Path("Section-3/21-sending_email/placeholder.jpg").read_bytes()))
 
 # Update SMTP host and port
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
