cb3e89154ce767488618017805c2beeec3a28442
MSA <crystalbluetech@gmail.com>
2024-12-26 09:37:06 +0100

    pipenv in vscode

--- Section-4/4-pipenv/app.py
+++ Section-4/4-pipenv/app.py
@@ -1,7 +1,10 @@
+"""
+This module demonstrates an example function that prints a message.
+"""
 import requests
 
 response = requests.get('https://api.github.com')
-print(response.status_code)
+print(response)
 
 # pipenv is equivalent to npm in node.js  or yarn in node.js or bundler in ruby
 # pipenv is a dependency manager for python projects
@@ -16,9 +19,9 @@ print(response.status_code)
 # pipenv install requests==2.30.0
 
 # pipenv install requests~=2.30.0  will create a Pipfile.lock file which will lock the version of the package to 2.30.x
-#  tto access the virtual environment and the path run the following command
+#  to access the virtual environment and the path run the following command
 # pipenv shell will activate the virtual environment
-# pipenv --venv will print the path to the virtual environment
+# pipenv --venv will print the path to the virtual environment directory
 
 
 # but remember we installed pipenv in the global environment so we can use it to install packages globally
--- app.py
+++ app.py
@@ -4,12 +4,8 @@ import requests
 
 # print(sys.argv)  # prints the list of command line arguments
 # # ['/Users/muhammadibnsalyhu/Desktop/CODING/learn_python/Section-3/23-cmd-line-args/app.py']
-# # the list must always have at least one element which is the name of the script
 
-if len(sys.argv) == 1:
-    print("USAGE: python app.py <password>")
-else:
-    password = sys.argv[1]
-    print(f"Password: {password}")
+response = requests.get('https://api.github.com')
+print(response)
     
     
